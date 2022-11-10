a5df05d769a61448479c0f58fb6062fccad258fd
RodrigoFantechi <rodrifante595@libero.it>
2022-11-10 14:42:15 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import SiteHeader from './components/SiteHeader.vue';
+import SiteMain from './components/SiteMain.vue';
+import Sitefooter from './components/SiteFooter.vue';
+
+export default {
+  components: {
+    SiteHeader,
+    SiteMain,
+    Sitefooter
+  }
+}
+</script>
+
+<template>
+  <SiteHeader />
+  <SiteMain />
+  <Sitefooter />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default{
+    name: 'SiteHeader'
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+
+</style>
